d84931a82fa829bb1fb98345380db50d0cf9687a
shin woojong <shinuj9966@gmail.com>
2023-07-19 09:47:44 +0900

    :tada: [init] backend 세팅

---
+++ backend/classmusic/src/main/java/com/quokka/classmusic/ClassmusicApplication.java
@@ -0,0 +1,13 @@
+package com.quokka.classmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmusicApplication.class, args);
+	}
+
+}
---
+++ backend/classmusic/src/test/java/com/quokka/classmusic/ClassmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quokka.classmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
