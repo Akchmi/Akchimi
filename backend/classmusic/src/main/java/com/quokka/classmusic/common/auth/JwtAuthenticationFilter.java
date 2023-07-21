03ea552e6375e99e20183312858f76533543c23d
phsaem <phsaem98@gmail.com>
2023-07-21 21:30:55 +0900

    :sparkles: JWT 토큰 기능 추가 중

---
+++ backend/classmusic/src/main/java/com/quokka/classmusic/common/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.quokka.classmusic.common.auth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.debug("JWT 필터 시작 Authorization 헤더 : {}", request.getHeader("Authorization"));
+
+        filterChain.doFilter(request, response);
+    }
+
+
+}
