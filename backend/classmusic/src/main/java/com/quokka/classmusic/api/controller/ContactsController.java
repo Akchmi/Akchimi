8712d2e81284e0eb3a9d2096fa2fa848b81b7f0a
roeyr <roeyr@naver.com>
2023-07-22 21:08:03 +0900

    :sparkles: Contact 목록 찾기

--- backend/classmusic/src/main/java/com/quokka/classmusic/api/controller/ContactsController.java
+++ backend/classmusic/src/main/java/com/quokka/classmusic/api/controller/ContactsController.java
@@ -7,6 +7,7 @@ import com.quokka.classmusic.api.response.ContactsListVo;
 import com.quokka.classmusic.api.service.ContactsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- backend/classmusic/src/main/java/com/quokka/classmusic/db/repository/ContactsRepositoryImpl.java
+++ backend/classmusic/src/main/java/com/quokka/classmusic/db/repository/ContactsRepositoryImpl.java
@@ -1,13 +1,11 @@
 package com.quokka.classmusic.db.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.quokka.classmusic.api.request.ContactsInsertDto;
-import com.quokka.classmusic.api.response.ContactsListVo;
 import com.quokka.classmusic.db.entity.Contact;
-import com.quokka.classmusic.db.entity.QContact;
 import com.quokka.classmusic.db.entity.Teacher;
 import com.quokka.classmusic.db.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -30,8 +28,25 @@ public class ContactsRepositoryImpl implements ContactsRepository {
 
     @Override
     public List<Contact> findAll(Map<String ,Integer> params) {
-        return query.select(contact).from(contact).fetch();
-//        return em.createQuery("select c.contactId from Contact c").getResultList();
+        Integer id = Integer.parseInt(String.valueOf(params.get("id")));
+        Integer state = Integer.parseInt(String.valueOf(params.get("state")));
+        Integer type = Integer.parseInt(String.valueOf(params.get("type")));
+        if(type == 1){
+            return query
+                    .select(contact)
+                    .from(contact)
+                    .where(contact.student.userId.eq(id) , contact.state.eq(state))
+                    .orderBy(contact.studentOrder.desc())
+                    .fetch();
+        } else if(type == 2){
+            return query
+                    .select(contact)
+                    .from(contact)
+                    .where(contact.teacher.teacherId.eq(id) , contact.state.eq(state))
+                    .orderBy(contact.teacherOrder.desc())
+                    .fetch();
+        }
+        return null;
     }
 
     @Override
@@ -58,11 +73,14 @@ public class ContactsRepositoryImpl implements ContactsRepository {
                 .teacher(teacher)
                 .state(1)
                 .build();
-
-
-
-
         em.persist(contact);
         return 1;
+    }
+
+    private BooleanExpression idEq(Integer id){
+        if(id == null){
+            return null;
+        }
+        return contact.student.userId.eq(id);
     }
 }
