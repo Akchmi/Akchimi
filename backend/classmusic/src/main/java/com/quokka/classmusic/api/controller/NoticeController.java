239df1136746d4c313b10f593022c36953fa526f
pH-Saem <phsaem98@gmail.com>
2023-07-23 22:55:26 +0900

    :sparkles: 공지사항 생성 시 작성자 정보 SpringSecurityContext에서 가져오는 기능

--- backend/classmusic/src/main/java/com/quokka/classmusic/api/controller/NoticeController.java
+++ backend/classmusic/src/main/java/com/quokka/classmusic/api/controller/NoticeController.java
@@ -3,11 +3,13 @@ package com.quokka.classmusic.api.controller;
 import com.quokka.classmusic.api.request.NoticeDto;
 import com.quokka.classmusic.api.response.NoticeDetailVo;
 import com.quokka.classmusic.api.response.NoticeListVo;
+import com.quokka.classmusic.api.response.UserDetailsVo;
 import com.quokka.classmusic.api.service.NoticeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -37,7 +39,10 @@ public class NoticeController {
     }
 
     @PostMapping
-    public ResponseEntity<Integer> insertNotice(@RequestBody NoticeDto noticeInsertDto){
+    public ResponseEntity<Integer> insertNotice(@RequestBody NoticeDto noticeInsertDto, @AuthenticationPrincipal UserDetailsVo userDetailsVo){
+        log.debug("POST /notices");
+        log.debug("공지사항 작성자 : {}", userDetailsVo);
+
         return new ResponseEntity<>(noticeService.insertNotice(noticeInsertDto), HttpStatus.ACCEPTED);
     }
 
