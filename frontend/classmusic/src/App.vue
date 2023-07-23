ddbc7792d00de64b212c211d7465a476c8e08f09
김주현 <rlawngus989898@gamil.com>
2023-07-23 19:08:41 +0900

    :art: 네비바/공지사항 리스트 추가

--- frontend/classmusic/src/App.vue
+++ frontend/classmusic/src/App.vue
@@ -3,16 +3,19 @@
     <nav class="navbar">
       <div class="navbar__logo">
         <i class="fas fa-blog"></i>
-        <a>Akchimi</a>
+        <a @click="$router.push('/')">Akchimi</a>
       </div>
 
       <ul class="navbar__menu">
-        <li><a>공지사항</a></li>
-        <li><a>자유게시판</a></li>
-        <li><a>강의실</a></li>
-        <li><a>강사검색</a></li>
-        <li><a>로그인</a></li>
-        <li><a>마이페이지</a></li>
+        <li @click="$router.push('/notice/list')">공지사항</li>
+        <li @click="$router.push('/article/list')">자유게시판</li>
+        <li @click="$router.push('/lecture/studentwaiting')">강의실</li>
+        <li @click="$router.push('/search')">강사검색</li>
+      </ul>
+
+      <ul class="navbar__menu">
+        <li @click="$router.push('/login/signin')">로그인</li>
+        <li @click="$router.push('/profile/myprofile')">마이페이지</li>
       </ul>
     </nav>
 
--- frontend/classmusic/src/components/Notice/NoticeList.vue
+++ frontend/classmusic/src/components/Notice/NoticeList.vue
@@ -1,9 +1,69 @@
 <template>
-  <div>noticelist</div>
+  <div>
+    <div class="container">
+      <!-- 공지사항 리스트 최상단 -->
+      <h1>공지사항</h1>
+      <hr />
+    </div>
+
+    <div class="container">
+      <!-- 공지사항 게시물 리스트 -->
+      <table>
+        <thead>
+          <tr>
+            <th>글 번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>작성일자</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="notice in notices" :key="notice.id">
+            <td>{{ notice.id }}</td>
+            <td>{{ notice.title }}</td>
+            <td>관리자</td>
+            <td>{{ notice.date }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+    </div>
+
+    <div class="container">
+      <!-- 공지사항 리스트 검색바 -->
+      <select v-model="selectedSearchCategory">
+        <option v-for="(item, idx) in searchCategory" :key="idx" :value="item">
+          {{ item }}
+        </option>
+      </select>
+      <input style="margin: 10px" type="text" v-model="searchQuery" />
+      <button>검색</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  data() {
+    return {
+      searchQuery: "",
+      selectedSearchCategory: "전체",
+    };
+  },
+  setup() {
+    const store = useStore();
+    const notices = computed(() => store.state.notices.notices);
+
+    const searchCategory = ["전체", "제목", "내용"];
+
+    return { notices, searchCategory };
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "@/assets/scss/notice.scss";
+</style>
