fb70f5b662eee867b102ea3857349dce48f75bba
cho1taeho <cow12dog@gmail.com>
2023-07-25 23:33:01 +0900

    :sparkles: 강사리뷰

--- frontend/classmusic/src/components/Profile/TeacherProfile.vue
+++ frontend/classmusic/src/components/Profile/TeacherProfile.vue
@@ -1,52 +1,61 @@
 <template>
   <div>
     teacherprofile
-    <br />
-    <button>
-      <router-link to="/profile/teacherprofileupdate"
-        >프로필 수정하기</router-link
-      >
-    </button>
-    <br />
-    <button>
-      <router-link to="/profile/teacherreview">리뷰보기</router-link>
-    </button>
 
     <!-- 남이 보는 강사 프로필 -->
-    <div class="teacher-profile">
-      <div class="top-section">
-        <img :src="image" alt="Teacher profile picture" class="teacher-image" />
-        <div class="teacher-name-gender-info">
-          <div class="name-gender-row">
-            <div class="name-box">
-              <p class="label">강사이름</p>
-              <span class="box">{{ name }}</span>
+    <div class="teacher-profile-container">
+      <div class="teacher-profile">
+        <div class="top-section">
+          <img :src="image" alt="Teacher profile picture" class="teacher-image" />
+          <div class="teacher-name-gender-info">
+            <div class="name-gender-row">
+              <div class="name-box">
+                <p class="label">강사이름</p>
+                <span class="box">{{ name }}</span>
+              </div>
+              <div class="gender-box">
+                <p class="label">성별</p>
+                <span class="box">{{ gender }}</span>
+              </div>
             </div>
-            <div class="gender-box">
-              <p class="label">성별</p>
-              <span class="box">{{ gender }}</span>
+            <div class="info-container">
+              <p class="info-box">악기 : {{ information.instrument }}</p>
+              <p class="info-box">경력 : {{ information.career }}</p>
+              <p class="info-box">가격 : {{ information.expense }}</p>
+              <p class="info-box">시간 : {{ information.time }}</p>
             </div>
           </div>
-          <div class="info-container">
-            <p class="info-box">악기 : {{ information.instrument }}</p>
-            <p class="info-box">경력 : {{ information.career }}</p>
-            <p class="info-box">가격 : {{ information.expense }}</p>
-            <p class="info-box">시간 : {{ information.time }}</p>
+        </div>
+        <div class="teacher-details">
+          <h2>자기 소개</h2>
+          <div class="description-box">
+            <p>{{ description }}</p>
           </div>
         </div>
-      </div>
-      <div class="teacher-details">
-        <h2>자기 소개</h2>
-        <div>
-          <p>{{ description }}</p>
+        <div class="attach-file">
+          <img :src="image" alt="Attached file" class="attach-image" />
+          <img :src="image" alt="Attached file" class="attach-image" />
+          <img :src="image" alt="Attached file" class="attach-image" />
+        </div>
+        <div class="button-group">
+          <router-link to="/" class="button">강사 즐겨찾기</router-link>
+          <router-link to="/lecture/studentwaiting" class="button">강의 신청</router-link>
         </div>
       </div>
+      <TeacherReview></TeacherReview>
     </div>
+ 
   </div>
 </template>
 
 <script>
+
+import TeacherReview from './TeacherReview.vue'
+
 export default {
+  components: {
+    TeacherReview
+  },
   props: {
     image: { type: String, default: "https://via.placeholder.com/280" },
     name: { type: String, default: "박한샘" },
@@ -61,21 +70,71 @@ export default {
       }),
     },
     description: { type: String, default: "이대나온여자" },
+    attachedFile: { type: String, default: "https://via.placeholder.com/280" },
   },
 };
 </script>
 
 <style scoped>
+
+.teacher-profile-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
+  align-items: center; 
+  height: 100vh; 
+}
+
+.button-group {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+}
+
+.button {
+  background: none;
+  border: none;
+  color: inherit;
+  text-align: inherit;
+  cursor: pointer;
+  padding: 0;
+  font: inherit;
+  outline: inherit;
+  text-decoration: none; 
+  margin-right: 10px; 
+}
+
+
+.attach-image {
+  width:170px;
+  height:220px;
+  object-fit: cover;
+}
+
+.description-box {
+  border: 1px solid black;
+  padding: 10px;
+  height: auto; 
+  min-height: 6em;
+  max-height: auto; 
+  overflow: auto; 
+  line-height: 1.2em; 
+  margin-bottom: 20px;
+  border-radius: 10px;
+}
+
 .introduce {
   border: 1px solid black;
   width: 30px;
 }
 
 .teacher-profile {
+  box-sizing: border-box;
+  position: relative;
   display: flex;
   flex-direction: column;
   width: 800px;
-  height: 800px;
+  height: 1000px;
   border: 1px solid black;
   margin-bottom: 20px;
   border-radius: 8px;
@@ -113,7 +172,7 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  flex-basis: 30%; /* from 40% to 30% */
+  flex-basis: 30%;
   text-align: center;
 }
 
@@ -121,7 +180,7 @@ export default {
   display: flex;
   font-size: 16px;
   border: 1px solid black;
-  width: 70px; /* from 70px to 50px */
+  width: 70px; 
   height: 24px;
   margin: 1px;
   border-radius: 10px;
@@ -133,7 +192,7 @@ export default {
   border: 1px solid black;
   border-radius: 10px;
   padding: 10px;
-  height: 140px; /* adjusted the height */
+  height: 140px; 
   justify-content: center;
   align-items: center;
 }
@@ -147,8 +206,8 @@ export default {
 }
 
 .label {
-  line-height: 1; /* 줄간격을 조절 */
-  margin: 0; /* 마진을 조절 */
-  padding: 0; /* 패딩을 조절 */
+  line-height: 1; 
+  margin: 0; 
+  padding: 0; 
 }
 </style>
--- frontend/classmusic/src/components/Profile/TeacherReview.vue
+++ frontend/classmusic/src/components/Profile/TeacherReview.vue
@@ -1,9 +1,93 @@
 <template>
-  <div>teacherreview</div>
+  <div class="teacher-review-container">
+    <div class="review-header">
+      <h3 class="review-title">강사 리뷰</h3>
+      <p class="avg-rating">평균 별점: {{ avgRating }}</p>
+    </div>
+
+    <div class="review-box">
+      <img :src="teacherImage" alt="Teacher profile picture" class="teacher-image" />
+      <div class="review-content">
+        <div class="student-info">
+          <p>학생 이름: {{ studentName }}</p>
+          <p>별점: {{ studentRating }}</p>
+          <p>수강 기간: {{ courseDuration }}</p>
+        </div>
+        <div class="review-text">
+          <p>{{ reviewText }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    teacherImage: {
+      type: String,
+      default: "https://via.placeholder.com/50",
+    },
+    avgRating: {
+      type: String,
+      default: "4.5",
+    },
+    studentName: {
+      type: String,
+      default: "박한샘",
+    },
+    studentRating: {
+      type: String,
+      default: "5",
+    },
+    courseDuration: {
+      type: String,
+      default: "6개월",
+    },
+    reviewText: {
+      type: String,
+      default: "최고의 강사입니다!",
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.teacher-review-container {
+  box-sizing: border-box;
+  width: 800px;
+  margin: 0 auto;
+}
+
+.review-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.review-box {
+  display: flex;
+  margin-top: 20px;
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 8px;
+}
+
+.teacher-image {
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+}
+
+.review-content {
+  margin-left: 20px;
+}
+
+.student-info p {
+  margin: 5px 0;
+}
+
+.review-text {
+  margin-top: 10px;
+}
+</style>
