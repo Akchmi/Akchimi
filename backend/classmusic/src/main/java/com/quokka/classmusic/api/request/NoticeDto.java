e8c7fe8cac7eceafe7813b82a7d733b5ca461e11
phsaem <phsaem98@gmail.com>
2023-07-28 17:30:34 +0900

    :sparkles: 공지사항 CRUD 기능 추가

--- backend/classmusic/src/main/java/com/quokka/classmusic/api/request/NoticeDto.java
+++ backend/classmusic/src/main/java/com/quokka/classmusic/api/request/NoticeDto.java
@@ -3,19 +3,19 @@ package com.quokka.classmusic.api.request;
 import com.quokka.classmusic.db.entity.Notice;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @NoArgsConstructor
 public class NoticeDto {
     private String title;
     private String content;
-    private int hit;
 
     public Notice toEntity(){
         return Notice.builder()
                 .title(this.title)
                 .content(this.content)
-                .hit(this.hit)
                 .build();
     }
 }
--- backend/classmusic/src/main/java/com/quokka/classmusic/db/repository/NoticeRepository.java
+++ backend/classmusic/src/main/java/com/quokka/classmusic/db/repository/NoticeRepository.java
@@ -4,7 +4,14 @@ import com.quokka.classmusic.db.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+public interface NoticeRepository {
+    List<Notice> findAll(Map<String, String> params);
+    Notice findNoticeById(int noticeId);
+    void save(Notice notice);
+    void delete(Notice notice);
 }
