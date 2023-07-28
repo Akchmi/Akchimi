e6b7bb6d8427a3d9c9e4252b662168a1add6e509
cho1taeho <cow12dog@gmail.com>
2023-07-28 10:51:29 +0900

    :sparkles: common 폴더 생성

--- frontend/classmusic/src/store/actions/profiles.js
+++ frontend/classmusic/src/store/actions/profiles.js
@@ -1,16 +1,71 @@
 export default {
-  // 필요 기능(회원, 강사 프로필)
-  //내 프로필 조회
-  //로그 아웃
-  //회원 정보 수정
-  //회원 탈퇴
-  //비밀번호 변경
-  //즐겨찾기 목록 조회
-  //즐겨찾기 삭제
-  // 필요 기능(강사 프로필)
-  //리뷰 목록 조회
-  //강사 상세 조회
-  //강사 프로필 추가
-  //강사 프로필 수정
-  //강사 프로필 삭제
+  actions: {
+    // 로그인 사용자 프로필 조회
+    fetchProfile({ commit }) {
+      axios.get('/api/user/profile')
+        .then(response => {
+          commit('SET_PROFILE', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  
+    // 회원 정보 수정
+    updateProfile({ commit }, profileData) {
+      axios.put('/api/user/profile', profileData)
+        .then(response => {
+          commit('SET_PROFILE', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  
+    // 회원 탈퇴
+    deleteUser({ commit }) {
+      axios.delete('/api/user/profile')
+        .then(() => {
+          commit('LOGOUT');
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  
+    // 비밀번호 변경
+    changePassword({ commit }, passwordData) {
+      axios.post('/api/user/password', passwordData)
+        .then(response => {
+          commit('SET_PROFILE', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  
+    // 즐겨찾기 목록 조회
+    fetchFavorites({ commit }) {
+      axios.get('/api/user/favorites')
+        .then(response => {
+          commit('SET_FAVORITES', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  
+    // 즐겨찾기 삭제
+    deleteFavorite({ commit }, favoriteId) {
+      axios.delete(`/api/user/favorites/${favoriteId}`)
+        .then(() => {
+          commit('REMOVE_FAVORITE', favoriteId);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+  },
+  
 };
