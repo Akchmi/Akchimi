056c26719161aa0eff66cfb1e66ce2657a71485f
phsaem <phsaem98@gmail.com>
2023-08-01 17:09:46 +0900

    ✨ jwt accessToken 헤더에 추가하는 기능

--- frontend/classmusic/src/api/axios.js
+++ frontend/classmusic/src/api/axios.js
@@ -1,9 +1,27 @@
 import axios from "axios";
 
-export default axios.create({
+const api = axios.create({
   baseURL: "http://localhost:8080",
 
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config)=>{
+    console.log("axios bearer token 설정")
+    const common = JSON.parse(localStorage.getItem("vuex")).common;
+    const accessToken = common.accessToken;
+    console.log(accessToken);
+
+    if(accessToken != null)
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error)=>{
+    return Promise.reject(error);
+  }
+)
+
+export default api;
