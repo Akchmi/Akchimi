27f0f930c173f7e8e147426dfa294f16f7c1b8f7
phsaem <phsaem98@gmail.com>
2023-08-04 15:55:29 +0900

    :fire: 사용하지 않는 Exception 클래스 삭제

--- backend/classmusic/src/main/java/com/quokka/classmusic/common/exception/NotAuthorExeception.java
+++
@@ -1,9 +0,0 @@
-package com.quokka.classmusic.common.exception;
-
-public class NotAuthorExeception extends RuntimeException{
-    public NotAuthorExeception() {
-    }
-    public NotAuthorExeception(String message) {
-        super(message);
-    }
-}
