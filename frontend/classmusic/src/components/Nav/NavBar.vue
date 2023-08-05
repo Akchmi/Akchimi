1f9f70a8624ecc9226c3e5cc0d7c1767ad2063ea
김주현 <rlawngus989898@gamil.com>
2023-08-05 15:10:49 +0900

    :art: 배경화면 제작

--- frontend/classmusic/src/components/Nav/NavBar.vue
+++ frontend/classmusic/src/components/Nav/NavBar.vue
@@ -9,17 +9,18 @@
       <ul class="navbar__menu">
         <li @click="$router.push('/notice/list')">공지사항</li>
         <li @click="$router.push('/article/list')">자유게시판</li>
-        <li @click="$router.push('/lecture/studentwaiting')">강의실</li>
+        <li @click="goLecture">강의실</li>
         <li @click="$router.push('/search')">강사검색</li>
       </ul>
 
       <ul class="navbar__menu">
-        <li v-if="isLoggedIn" @click="$router.push('/profile/myprofile')">마이페이지</li>
+        <li v-if="isLoggedIn" @click="$router.push('/profile/myprofile')">
+          마이페이지
+        </li>
         <li v-if="isLoggedIn" @click="logOut">로그아웃</li>
         <li v-else @click="$router.push('/login/signin')">로그인</li>
-      </ul>      
+      </ul>
     </nav>
-
   </div>
 </template>
 
@@ -31,24 +32,32 @@ export default {
   data() {
     return {
       // isLoggedIn: false
-    }
+    };
   },
-    computed: {
+  computed: {
     isLoggedIn() {
       return this.$store.state.common.isLogin;
-    }
+    },
   },
   methods: {
     logOut() {
-      this.$store.commit('LOGOUT');
-      this.$router.push('/');
-    }
+      this.$store.commit("LOGOUT");
+      this.$router.push("/");
+    },
+
+    goLecture() {
+      if (!this.$store.getters.getIsLogin) {
+        alert("로그인이 필요한 서비스입니다.");
+        this.$router.push("/login/signin");
+        return;
+      }
+      this.$router.push("/lecture/studentwaiting");
+    },
   },
   created() {
     this.isLoggedIn = this.$store.state.common.isLogin;
   },
-
-}
+};
 </script>
 
 <style scoped>
--- frontend/classmusic/src/views/HomeView.vue
+++ frontend/classmusic/src/views/HomeView.vue
@@ -1,25 +1,48 @@
-<template>   
+<template>
   <div>
     <div>
       <NavBar></NavBar>
     </div>
     <router-view></router-view>
-</div>
-
+    <div class="quokka__div">
+      <div class="quokka__indiv">
+        <img src="@/assets/images/quokkaband.png" alt="" class="quokka" />
+        <h1>"악기라는 취미에 미치다" - 쿼카in</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import NavBar from "@/components/Nav/NavBar.vue"
-
+import NavBar from "@/components/Nav/NavBar.vue";
 
 export default {
   components: {
-    NavBar
-  }
-
+    NavBar,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/templates/common.scss";
+</style>
+
+<style>
+.quokka__div {
+  display: flex;
+  justify-content: center;
+  margin: 100px;
+  text-align: center;
+}
+
+.quokka__indiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.quokka {
+  width: 650px;
+  height: 650px;
+}
 </style>
