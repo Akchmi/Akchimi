5fa2b70203c33ded5e0dda9ed05d62a8ed291283
phsaem <phsaem98@gmail.com>
2023-08-07 15:43:35 +0900

    🐛 로그인 시 수정된 상태코드 반영

--- frontend/classmusic/src/store/actions/login.js
+++ frontend/classmusic/src/store/actions/login.js
@@ -2,22 +2,21 @@ import { apiLogin, apiFindId, apiFindPw } from "@/api/auth.js";
 import { HttpStatusCode } from "axios";
 
 export default {
-  async login({commit}, loginInfo) {    
-      const response = await apiLogin(loginInfo);
-      const { status, data } = response;
-      console.log(`로그인 요청 결과 : ${status}`);
-      
-      if(status == HttpStatusCode.Ok){
-        // 로그인 성공 시 vuex common에 저장
-        commit('SET_TOKEN', data, {root: true});
-   
-      }else if(status == HttpStatusCode.Unauthorized){
-        alert("비밀번호가 일치하지 않습니다.");
-        throw response;
-      }else if(status == HttpStatusCode.NotFound){
-        alert("아이디가 존재하지 않습니다.");
-        throw response;
-      }
+  async login({ commit }, loginInfo) {
+    const response = await apiLogin(loginInfo);
+    const { status, data } = response;
+    console.log(`로그인 요청 결과 : ${status}`);
+
+    if (status == HttpStatusCode.Ok) {
+      // 로그인 성공 시 vuex common에 저장
+      commit("SET_TOKEN", data, { root: true });
+    } else if (status == HttpStatusCode.BadRequest) {
+      alert("비밀번호가 일치하지 않습니다.");
+      throw response;
+    } else if (status == HttpStatusCode.NotFound) {
+      alert("아이디가 존재하지 않습니다.");
+      throw response;
+    }
   },
 
   async findId({ commit }, namemail) {
