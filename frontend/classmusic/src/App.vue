5af4eaacaa4e89d23c9da086b3c3653adae26ee4
pH-Saem <phsaem98@gmail.com>
2023-08-12 15:49:41 +0900

    🩹 reset.css import 순서 변경

--- frontend/classmusic/src/App.vue
+++ frontend/classmusic/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <!-- 구성 화면 -->
-    <div class="container">
-      <router-view></router-view>
-    </div>
-  </div>
+	<div>
+		<!-- 구성 화면 -->
+		<div class="container">
+			<router-view></router-view>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -12,23 +12,23 @@
 // import { useStore } from "vuex";
 
 export default {
-  setup() {
-    // const store = useStore();
-  },
+	setup() {
+		// const store = useStore();
+	},
 };
 </script>
 "
 
 <style lang="scss">
-@import "@/assets/scss/templates/common.scss";
 @import "@/assets/scss/reset.scss";
+@import "@/assets/scss/templates/common.scss";
 
 @font-face {
-  font-family: "mainfont";
-  src: url("assets/font/Maplestory.ttf") format("truetype");
+	font-family: "mainfont";
+	src: url("assets/font/Maplestory.ttf") format("truetype");
 }
 
 .container {
-  font-family: "mainfont";
+	font-family: "mainfont";
 }
 </style>
