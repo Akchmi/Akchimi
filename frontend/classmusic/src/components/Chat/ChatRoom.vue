2f3e3dc6f3ad1893184ec9e9fe03436f4511f279
pH-Saem <phsaem98@gmail.com>
2023-08-12 15:51:00 +0900

    🔥 불필요한 import 문 삭제

--- frontend/classmusic/src/components/Chat/ChatRoom.vue
+++ frontend/classmusic/src/components/Chat/ChatRoom.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
-    <div class="chat-room" ref="chatroom">
-      <!-- 채팅 목록 -->
-      <TheChat
-        v-for="(chat, idx) in chats"
-        :key="idx"
-        :chat="chat"
-        :profile="
-          chat.sender == '0' ? this.studentProfile : this.teacherProfile
-        "
-        :sender="sender"
-      />
-    </div>
-    <!-- 채팅 폼 -->
-    <div class="chat-input">
-      <input
-        class="chat-content"
-        v-model="content"
-        type="text"
-        @keyup.enter="sendMessage"
-      />
-      <button class="chat-button" @click="sendMessage">전송</button>
-    </div>
-  </div>
+	<div>
+		<div class="chat-room" ref="chatroom">
+			<!-- 채팅 목록 -->
+			<TheChat
+				v-for="(chat, idx) in chats"
+				:key="idx"
+				:chat="chat"
+				:profile="
+					chat.sender == '0'
+						? this.studentProfile
+						: this.teacherProfile
+				"
+				:sender="sender"
+			/>
+		</div>
+		<!-- 채팅 폼 -->
+		<div class="chat-input">
+			<input
+				class="chat-content"
+				v-model="content"
+				type="text"
+				@keyup.enter="sendMessage"
+			/>
+			<button class="chat-button" @click="sendMessage">전송</button>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -35,142 +37,141 @@ import { mapActions } from "vuex";
 // import A210URL from "@/api/axios";
 
 export default {
-  data() {
-    return {
-      roomId: null,
-      sender: null,
-      content: "",
+	data() {
+		return {
+			roomId: null,
+			sender: null,
+			content: "",
 
-      studentProfile: {
-        name: "",
-        profileImage: "",
-      },
-      teacherProfile: {
-        name: "",
-        profileImage: "",
-      },
-      chats: [],
-    };
-  },
-  components: {
-    TheChat,
-  },
+			studentProfile: {
+				name: "",
+				profileImage: "",
+			},
+			teacherProfile: {
+				name: "",
+				profileImage: "",
+			},
+			chats: [],
+		};
+	},
+	components: {
+		TheChat,
+	},
 
-  created() {
-    const route = useRoute();
-    this.roomId = route.params.lectureId;
-    this.sender = route.query.type;
+	created() {
+		const route = useRoute();
+		this.roomId = route.params.lectureId;
+		this.sender = route.query.type;
 
-    // App.vue가 생성되면 소켓 연결을 시도합니다.
-    this.connect();
+		// App.vue가 생성되면 소켓 연결을 시도합니다.
+		this.connect();
 
-    // 채팅방 초기 설정 (참여자 및 이전 채팅 불러오기)
-    this.initializeChatRoom();
-    this.scrollToBottom();
-  },
-  updated() {
-    this.scrollToBottom();
-  },
+		// 채팅방 초기 설정 (참여자 및 이전 채팅 불러오기)
+		this.initializeChatRoom();
+		this.scrollToBottom();
+	},
+	updated() {
+		this.scrollToBottom();
+	},
 
-  methods: {
-    ...mapActions(["postChatCreate"]),
-    async getChatLog() {
-      try {
-        this.chats = await apiGetChatLog(this.roomId);
+	methods: {
+		...mapActions(["postChatCreate"]),
+		async getChatLog() {
+			try {
+				this.chats = await apiGetChatLog(this.roomId);
 
-        console.log("getChatLog : ", this.chats);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    scrollToBottom() {
-      console.log("내려가");
-      this.$nextTick(() => {
-        const chatroom = this.$refs.chatroom;
-        chatroom.scrollTop = chatroom.scrollHeight;
-      });
-    },
+				console.log("getChatLog : ", this.chats);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		scrollToBottom() {
+			console.log("내려가");
+			this.$nextTick(() => {
+				const chatroom = this.$refs.chatroom;
+				chatroom.scrollTop = chatroom.scrollHeight;
+			});
+		},
 
-    async initializeChatRoom() {
-      // 채팅 참여자 정보 조회
-      await this.initializeChatParticipant();
+		async initializeChatRoom() {
+			// 채팅 참여자 정보 조회
+			await this.initializeChatParticipant();
 
-      // 이전 채팅 정보 조회
-      await this.getChatLog();
-    },
+			// 이전 채팅 정보 조회
+			await this.getChatLog();
+		},
 
-    async initializeChatParticipant() {
-      const participantInfo = await apiGetChatParticipant(this.roomId);
-      this.teacherProfile = {
-        name: participantInfo.teacherName,
-        profileImage: participantInfo.teacherProfileImage,
-      };
-      this.studentProfile = {
-        name: participantInfo.studentName,
-        profileImage: participantInfo.studentProfileImage,
-      };
-    },
+		async initializeChatParticipant() {
+			const participantInfo = await apiGetChatParticipant(this.roomId);
+			this.teacherProfile = {
+				name: participantInfo.teacherName,
+				profileImage: participantInfo.teacherProfileImage,
+			};
+			this.studentProfile = {
+				name: participantInfo.studentName,
+				profileImage: participantInfo.studentProfileImage,
+			};
+		},
 
-    save(newChat) {
-      // 채팅 메세지 DB에 추가
-      this.postChatCreate(newChat);
-    },
+		save(newChat) {
+			// 채팅 메세지 DB에 추가
+			this.postChatCreate(newChat);
+		},
 
-    sendMessage() {
-      if (this.userName !== "" && this.message !== "") {
-        const newChat = {
-          content: this.content,
-          sender: this.sender,
-          roomId: this.roomId,
-        };
-        this.send(newChat);
-        this.save(newChat);
-        this.content = "";
-      }
-    },
+		sendMessage() {
+			if (this.userName !== "" && this.message !== "") {
+				const newChat = {
+					content: this.content,
+					sender: this.sender,
+					roomId: this.roomId,
+				};
+				this.send(newChat);
+				this.save(newChat);
+				this.content = "";
+			}
+		},
 
-    send(newChat) {
-      console.log("Send message:" + this.content);
-      if (this.stompClient && this.stompClient.connected) {
-        this.stompClient.send("/receive", JSON.stringify(newChat), {});
-      }
-    },
+		send(newChat) {
+			console.log("Send message:" + this.content);
+			if (this.stompClient && this.stompClient.connected) {
+				this.stompClient.send("/receive", JSON.stringify(newChat), {});
+			}
+		},
 
-    connect() {
-      const serverURL = process.env.VUE_APP_API_URL + "/websocket";
-      let socket = new SockJS(serverURL);
-      this.stompClient = Stomp.over(socket);
-      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`);
-      this.stompClient.connect(
-        {},
-        (frame) => {
-          // 소켓 연결 성공
-          this.connected = true;
-          console.log("소켓 연결 성공", frame);
-          // 서버의 메시지 전송 endpoint를 구독합니다.
+		connect() {
+			const serverURL = process.env.VUE_APP_API_URL + "/websocket";
+			let socket = new SockJS(serverURL);
+			this.stompClient = Stomp.over(socket);
+			console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`);
+			this.stompClient.connect(
+				{},
+				(frame) => {
+					// 소켓 연결 성공
+					this.connected = true;
+					console.log("소켓 연결 성공", frame);
+					// 서버의 메시지 전송 endpoint를 구독합니다.
 
-          this.stompClient.subscribe("/send", (res) => {
-            console.log("구독으로 받은 메시지 입니다.", res.body);
-            const livemessage = JSON.parse(res.body);
-            if (livemessage.roomId == this.roomId) {
-              livemessage.createdTime = new Date() / 1000;
+					this.stompClient.subscribe("/send", (res) => {
+						console.log("구독으로 받은 메시지 입니다.", res.body);
+						const livemessage = JSON.parse(res.body);
+						if (livemessage.roomId == this.roomId) {
+							livemessage.createdTime = new Date() / 1000;
 
-              this.chats.push(livemessage);
-            }
-          });
-        },
-        (error) => {
-          // 소켓 연결 실패
-          console.log("소켓 연결 실패", error);
-          this.connected = false;
-        }
-      );
-    },
-  },
+							this.chats.push(livemessage);
+						}
+					});
+				},
+				(error) => {
+					// 소켓 연결 실패
+					console.log("소켓 연결 실패", error);
+					this.connected = false;
+				}
+			);
+		},
+	},
 };
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/templates/common.scss";
 @import "@/assets/scss/chat.scss";
 </style>
--- frontend/classmusic/src/components/Chat/TheChat.vue
+++ frontend/classmusic/src/components/Chat/TheChat.vue
@@ -1,10 +1,12 @@
 <template>
-  <div :class="chat.sender == sender ? 'right' : 'left'">
-    <!-- <div class="chat-name">보낸이: {{ profile.name }}</div> -->
-    <img class="chat-img" :src="profile.profileImage" />
-    <span class="chat-content"> {{ chat.content }}</span>
-    <span class="chat-sendtime">{{ toLocalTimeStamp(chat.createdTime) }}</span>
-  </div>
+	<div :class="chat.sender == sender ? 'right' : 'left'">
+		<!-- <div class="chat-name">보낸이: {{ profile.name }}</div> -->
+		<img class="chat-img" :src="profile.profileImage" />
+		<span class="chat-content"> {{ chat.content }}</span>
+		<span class="chat-sendtime">{{
+			toLocalTimeStamp(chat.createdTime)
+		}}</span>
+	</div>
 </template>
 
 <script>
@@ -12,45 +14,44 @@ import { mapActions } from "vuex";
 import utils from "@/common/utils";
 
 export default {
-  data() {
-    return {};
-  },
-  props: {
-    chat: Object,
-    profile: Object,
-    sender: String,
-  },
-  created() {},
+	data() {
+		return {};
+	},
+	props: {
+		chat: Object,
+		profile: Object,
+		sender: String,
+	},
+	created() {},
 
-  methods: {
-    ...mapActions(["postChatCreate"]),
+	methods: {
+		...mapActions(["postChatCreate"]),
 
-    toLocalTimeStamp(unixTimeStamp) {
-      return utils.unixTimeStampToLocalTimeStamp(unixTimeStamp);
-    },
-  },
+		toLocalTimeStamp(unixTimeStamp) {
+			return utils.unixTimeStampToLocalTimeStamp(unixTimeStamp);
+		},
+	},
 };
 </script>
 
 <style lang="scss" scoped>
-@import "@/assets/scss/templates/common.scss";
 @import "@/assets/scss/chat.scss";
 img {
-  width: 50px;
-  border-radius: 70%;
+	width: 50px;
+	border-radius: 70%;
 }
 
 .left {
-  display: flex;
-  flex-direction: row;
-  justify-content: left;
-  align-items: center;
+	display: flex;
+	flex-direction: row;
+	justify-content: left;
+	align-items: center;
 }
 
 .right {
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: right;
-  align-items: center;
+	display: flex;
+	flex-direction: row-reverse;
+	justify-content: right;
+	align-items: center;
 }
 </style>
