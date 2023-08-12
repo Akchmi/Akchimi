51c2926d48176dec8e4f1e633e3c68e51ffee0f1
pH-Saem <phsaem98@gmail.com>
2023-08-12 21:51:19 +0900

    ⚡️ 에러페이지 뷰 생성 및 라우터 수정

---
+++ frontend/classmusic/src/views/ErrorView.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div>
+			<NavBar></NavBar>
+		</div>
+		<TheBanner :title="'사소한 문제가 발생했습니다.'" />
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import NavBar from "@/components/common/NavBar.vue";
+import TheBanner from "@/components/common/TheBanner";
+
+export default {
+	name: "ErrorView",
+	data() {
+		return {};
+	},
+	components: {
+		NavBar,
+		TheBanner,
+	},
+
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
