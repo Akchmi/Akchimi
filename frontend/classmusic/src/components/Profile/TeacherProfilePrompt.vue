87a39d096db312f8f1ca011dc5c681a4be2b4d94
pH-Saem <phsaem98@gmail.com>
2023-08-13 10:47:51 +0900

    💄 강사 프로필 등록하러 가기 CSS 수정

--- frontend/classmusic/src/components/Profile/TeacherProfilePrompt.vue
+++ frontend/classmusic/src/components/Profile/TeacherProfilePrompt.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="teacher-registration-box">
-    <p>아직 강사 등록을 하지 않으셨군요?</p>
-    <button>
-      <router-link to="/profile/teacherprofilecreate"
-        >강사 등록하러 가기</router-link
-      >
-    </button>
-  </div>
+	<div class="teacher-registration-box">
+		<h3 class="teacher-registration-message">아직 강사 등록을 하지 않으셨군요?</h3>
+		<button @click="this.$router.push('/profile/teacherprofilecreate')">
+			강사 등록하러 가기
+		</button>
+	</div>
 </template>
 
 <script>
@@ -14,20 +12,24 @@ export default {};
 </script>
 
 <style scoped>
+.teacher-registration-message {
+	margin-bottom: 20px;
+}
+
 .teacher-registration-box {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 40%;
-  height: 60%;
-  transform: translate(-60%, -20%);
-  border: 1.4px solid #edd9b7;
-  border-radius: 0.25rem;
-  padding: 20px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+	position: absolute;
+	top: 40%;
+	left: 50%;
+	width: 40%;
+	height: 40%;
+	transform: translate(-60%, -20%);
+	border: 1.4px solid #edd9b7;
+	border-radius: 0.25rem;
+	padding: 20px;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 }
 </style>
